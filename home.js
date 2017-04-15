4a92da399ffdf75fc76e1ca5c37edd0d83283704
zsarlenealyzagarcia <alyzang1118@gmail.com>
2017-04-15 10:01:33 +0800

    added home.js

---
+++ home.js
@@ -0,0 +1,35 @@
+window.onload = function()
+{	
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// for the proceed button on the modal
+var sbtn = document.getElementById("secBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+    document.getElementById('namedisplay').innerHTML = document.getElementById('name').value;
+    document.getElementById('iddisplay').innerHTML = document.getElementById('id').value;
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+}
+
