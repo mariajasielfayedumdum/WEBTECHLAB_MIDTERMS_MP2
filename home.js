aee8a7feb77456701a3fe09f8a215084da0ad4b1
faye <2153202@slu.edu.ph>
2017-05-01 00:26:02 +0800

    update on the home page
    usage of appcache to make the web app offline

--- home.js
+++ home.js
@@ -18,6 +18,24 @@ btn.onclick = function() {
     document.getElementById('namedisplay').innerHTML = document.getElementById('name').value;
     document.getElementById('iddisplay').innerHTML = document.getElementById('id').value;
 }
+sbtn.onclick = function () {
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://webtek2017/mp2/home.json", true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	var name = document.getElementById('name').value;
+	var ccode = document.getElementById('classcode').value;
+	var facId = document.getElementById('id').value;
+	var arr = {
+		"name": name,
+		"id": facId,
+		"classcode": ccode,
+	};
+	var data = JSON.stringify(arr);
+	xhr.send(data);
+
+	window.location.href = "second.html";
+
+}
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
@@ -30,6 +48,54 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+var appCache = window.applicationCache;
+
+switch (appCache.status) {
+  case appCache.UNCACHED: // UNCACHED == 0
+    return 'UNCACHED';
+    break;
+  case appCache.IDLE: // IDLE == 1
+    return 'IDLE';
+    break;
+  case appCache.CHECKING: // CHECKING == 2
+    return 'CHECKING';
+    break;
+  case appCache.DOWNLOADING: // DOWNLOADING == 3
+    return 'DOWNLOADING';
+    break;
+  case appCache.UPDATEREADY:  // UPDATEREADY == 4
+    return 'UPDATEREADY';
+    break;
+  case appCache.OBSOLETE: // OBSOLETE == 5
+    return 'OBSOLETE';
+    break;
+  default:
+    return 'UKNOWN CACHE STATUS';
+    break;
+};
+
+appCache.update(); // Attempt to update the user's cache.
+
+if (appCache.status == window.applicationCache.UPDATEREADY) {
+  appCache.swapCache();  // The fetch was successful, swap in the new cache.
+}
+
+// Check if a new cache is available on page load.
+window.addEventListener('load', function(e) {
+
+  window.applicationCache.addEventListener('updateready', function(e) {
+    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+      // Browser downloaded a new app cache.
+      if (confirm('A new version of this site is available. Load it?')) {
+        window.location.reload();
+      }
+    } else {
+      // Manifest didn't changed. Nothing new to server.
+    }
+  }, false);
+
+}, false);
 
 }
 
